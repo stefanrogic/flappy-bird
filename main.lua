6eafe4e99e441be9017fd38c26ef45ee1e105205
stefanrogic <stefanrogic@protonmail.com>
2026-01-30 11:57:56 +0100

    initial window setup and import of necessary libraries

--- main.lua
+++ main.lua
@@ -1,8 +1,27 @@
 Class = require 'class'
 push = require 'push'
 
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+local birdImage = love.graphics.newImage('sprites/bird.png')
+local pipeImage = love.graphics.newImage('sprites/pipe.png')
+local backgroundImage = love.graphics.newImage('sprites/background.png')
+local groundImage = love.graphics.newImage('sprites/ground.png')
+
 function love.load()
-    -- Initialize game variables and assets here
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle("Flappy Bird")
+    love.window.setIcon(love.image.newImageData('sprites/bird.png'))
+    
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = false
+    })
 end
 
 function love.update(dt)
